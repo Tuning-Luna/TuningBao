2c0accf48689cc98589df35dbda0f9a61b61b8ea
TuningLuna <2081672492@qq.com>
2025-07-16 09:56:49 +0800

    feat(home):实现轮播图效果

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -6,6 +6,13 @@
 		</view>
 
 
+		<swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="i in 5" :key="i">
+				<image :src="`/static/home/swiperImg/${i}.png`"></image>
+			</swiper-item>
+		</swiper>
+
+
 	</view>
 </template>
 
@@ -22,6 +29,25 @@
 			position: sticky;
 			top: 0;
 			z-index: 10;
+		}
+
+		.swiper {
+			width: 100%;
+			height: 200px;
+			background-color: $tuning-main-color;
+
+			swiper-item {
+				width: 100%;
+				height: 200px;
+
+				image {
+					width: 100%;
+					height: 200px;
+					display: block;
+
+					border-radius: 10px;
+				}
+			}
 		}
 	}
 </style>
